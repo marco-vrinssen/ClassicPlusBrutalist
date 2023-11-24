bff3d4d1f56df78cad03e608177be6f34219aafb
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2023-11-24 19:08:43 +0100

    Updating the aura event frame

--- auras.lua
+++ auras.lua
@@ -4,6 +4,9 @@ local function PlayerAurasUpdate()
     BuffFrame:SetHeight(8)
 end
 
-local PlayerAurasEventFrame = CreateFrame("FRAME")
+local PlayerAurasEventFrame = CreateFrame("Frame")
 PlayerAurasEventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+PlayerAurasEventFrame:RegisterEvent("ZONE_CHANGED")
+PlayerAurasEventFrame:RegisterEvent("ZONE_CHANGED_INDOORS")
+PlayerAurasEventFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 PlayerAurasEventFrame:SetScript("OnEvent", PlayerAurasUpdate)
